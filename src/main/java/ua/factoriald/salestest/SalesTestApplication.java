44564d5e8e2ecfdc9069cc6591971e33c1a37912
Salahor Dmytro <factoriald@gmail.com>
2023-03-28 22:49:59 +0300

    this is all I can do in time

--- src/main/java/ua/factoriald/salestest/SalesTestApplication.java
+++ src/main/java/ua/factoriald/salestest/SalesTestApplication.java
@@ -2,9 +2,10 @@ package ua.factoriald.salestest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SalesTestApplication {
+public class SalesTestApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SalesTestApplication.class, args);
